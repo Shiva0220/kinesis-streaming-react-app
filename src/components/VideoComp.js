f30a075409bc574df8a3834a66933d1660bdbc57
ShivaKumar <shivakumar@beehyv.com>
2021-02-25 23:55:15 +0530

    Fixed the issues with ui.js

--- src/components/VideoComp.js
+++ src/components/VideoComp.js
@@ -5,27 +5,27 @@ export const VideoComp = (props) => {
     return <div className="col-md-8">
         <div id="playerContainer">
 
-            <video id="hlsjs" className="player" controls autoPlay></video>
-            <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
+            <video id="hlsjs" className="player" controls autoPlay/>
+            {/*<script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>*/}
 
-            <video id="videojs" className="player video-js vjs-default-skin" controls autoPlay></video>
+            <video id="videojs" className="player video-js vjs-default-skin" controls autoPlay/>
             <link rel="stylesheet" href="https://vjs.zencdn.net/6.6.3/video-js.css"/>
-            <script src="https://vjs.zencdn.net/6.6.3/video.js"></script>
-            <script
-                src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.14.1/videojs-contrib-hls.js"></script>
+            {/*<script src="https://vjs.zencdn.net/6.6.3/video.js"></script>*/}
+            {/*<script*/}
+            {/*    src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.14.1/videojs-contrib-hls.js"></script>*/}
 
-            <video id="shaka" className="player" controls autoPlay></video>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/shaka-player/2.4.1/shaka-player.compiled.js">
-            </script>
+            <video id="shaka" className="player" controls autoPlay/>
+            {/*<script src="https://cdnjs.cloudflare.com/ajax/libs/shaka-player/2.4.1/shaka-player.compiled.js">*/}
+            {/*</script>*/}
 
-            <video id="dashjs" className="player" controls autoPlay=""></video>
-            <script src="https://cdn.dashjs.org/latest/dash.all.min.js"></script>
+            <video id="dashjs" className="player" controls autoPlay=""/>
+            {/*<script src="https://cdn.dashjs.org/latest/dash.all.min.js"/>*/}
         </div>
 
-        {/*<h3 style= "margin-top: 20px;">Logs</h3>*/}
-        {/*<div className="card bg-light mb-3">*/}
-        {/*    <pre id="logs" className="card-body text-monospace"*/}
-        {/*         style="font-family: monospace; white-space: pre-wrap;"></pre>*/}
-        {/*</div>*/}
+        <h3 style={{"marginTop": "20px"}}>Logs</h3>
+        <div className="card bg-light mb-3">
+            <pre id="logs" className="card-body text-monospace"
+                 style={{"fontFamily": "monospace", "whiteSpace": "pre-wrap"}}/>
+        </div>
     </div>;
 }
--- src/ui.js
+++ src/ui.js
@@ -1,111 +1,108 @@
-// import AWS from '../node_modules/aws-sdk'
-// import $ from '../node_modules/jquery'
-// import Hls from '../node_modules/hls/'
-// import shaka from '../node_modules/shaka-player'
-// import video from '../node_modules/video'
-// import videojs from '../node_modules/videojs-contrib-hls'
-// import '../../ui'
-// import dashjs from '../node_modules/dash/'
-//
-//
-// configureLogging();
-//
-// // Update all of the corresonding fields when switching protocol or playback mode
-// $('#protocol').change(function() {
-//     updateFieldsCorrespondingToStreamingProtocol();
-// });
-// $('#playbackMode').change(function() {
-//     updateFieldsCorrespondingToPlaybackMode();
-// });
-//
-// // Read/Write all of the fields to/from localStorage so that fields are not lost on refresh.
-// [
-//     'protocol',
-//     'player',
-//     'region',
-//     'accessKeyId',
-//     'secretAccessKey',
-//     'sessionToken',
-//     'endpoint',
-//     'streamName',
-//     'playbackMode',
-//     'discontinuityMode',
-//     'displayFragmentTimestamp',
-//     'displayFragmentNumber',
-//     'startTimestamp',
-//     'endTimestamp',
-//     'fragmentSelectorType',
-//     'maxResults',
-//     'expires'
-// ].forEach(function(field) {
-//     var id = "#" + field;
-//
-//     // Read field from localStorage
-//     try {
-//         var localStorageValue = localStorage.getItem(field);
-//         if (localStorageValue) {
-//             $(id).val(localStorageValue);
-//             $(id).trigger('change');
-//         }
-//     } catch (e) { /* Don't use localStorage */ }
-//
-//     // Write field to localstorage on change event
-//     $(id).change(function() {
-//         try {
-//             localStorage.setItem(field, $(id).val());
-//         } catch (e) { /* Don't use localStorage */ }
-//     });
-// });
-//
-// // Initially hide the player elements
-// $('.player').hide();
-//
-// function configureLogging() {
-//     console._error = console.error;
-//     console.error = function(messages) {
-//         log('ERROR', Array.prototype.slice.call(arguments));
-//         console._error.apply(this, arguments);
-//     }
-//
-//     console._log = console.log;
-//     console.log = function(messages) {
-//         log('INFO', Array.prototype.slice.call(arguments));
-//         console._log.apply(this, arguments);
-//     }
-//
-//     function log(level, messages) {
-//         var text = '';
-//         for (message of messages) {
-//             if (typeof message === 'object') { message = JSON.stringify(message, null, 2); }
-//             text += ' ' + message;
-//         }
-//         $('#logs').append($('<div>').text('[' + level + ']' + text + '\n'));
-//     }
-// }
-//
-// function updateFieldsCorrespondingToStreamingProtocol() {
-//     var isDASH = $('#protocol').val() === 'DASH';
-//     $('#containerFormat').prop('disabled', isDASH);
-//     $('#discontinuityMode').prop('disabled', isDASH);
-//     $('#displayFragmentNumber').prop('disabled', !isDASH);
-//
-//     var players = isDASH ? DASH_PLAYERS : HLS_PLAYERS;
-//     $('#player').empty();
-//     players.forEach(function (player) {
-//         var option = $('<option>').text(player);
-//         $('#player').append(option);
-//     });
-//     $('#player').trigger('change');
-// }
-// updateFieldsCorrespondingToStreamingProtocol();
-//
-// function updateFieldsCorrespondingToPlaybackMode() {
-//     var isLive = $('#playbackMode').val() === 'LIVE';
-//     $('#startTimestamp').prop('disabled', isLive);
-//     $('#endTimestamp').prop('disabled', isLive);
-// }
-// updateFieldsCorrespondingToPlaybackMode();
-//
-// $('.loader').hide();
-// $('.main').show();
-// console.log("Page loaded")
+import $ from 'jquery'
+
+
+configureLogging();
+
+// Update all of the corresonding fields when switching protocol or playback mode
+$('#protocol').change(function() {
+    updateFieldsCorrespondingToStreamingProtocol();
+});
+$('#playbackMode').change(function() {
+    updateFieldsCorrespondingToPlaybackMode();
+});
+
+// Read/Write all of the fields to/from localStorage so that fields are not lost on refresh.
+[
+    'protocol',
+    'player',
+    'region',
+    'accessKeyId',
+    'secretAccessKey',
+    'sessionToken',
+    'endpoint',
+    'streamName',
+    'playbackMode',
+    'discontinuityMode',
+    'displayFragmentTimestamp',
+    'displayFragmentNumber',
+    'startTimestamp',
+    'endTimestamp',
+    'fragmentSelectorType',
+    'maxResults',
+    'expires'
+].forEach(function(field) {
+    var id = "#" + field;
+
+    // Read field from localStorage
+    try {
+        var localStorageValue = localStorage.getItem(field);
+        if (localStorageValue) {
+            $(id).val(localStorageValue);
+            $(id).trigger('change');
+        }
+    } catch (e) { /* Don't use localStorage */ }
+
+    // Write field to localstorage on change event
+    $(id).change(function() {
+        try {
+            localStorage.setItem(field, $(id).val());
+        } catch (e) { /* Don't use localStorage */ }
+    });
+});
+
+// Initially hide the player elements
+$('.player').hide();
+
+function configureLogging() {
+    console._error = console.error;
+    console.error = function(messages) {
+        log('ERROR', Array.prototype.slice.call(arguments));
+        console._error.apply(this, arguments);
+    }
+
+    console._log = console.log;
+    console.log = function(messages) {
+        log('INFO', Array.prototype.slice.call(arguments));
+        console._log.apply(this, arguments);
+    }
+
+    function log(level, messages) {
+        var text = '';
+        messages.map((message) => {
+            if (typeof message === 'object') {
+                message = JSON.stringify(message, null, 2);
+            }
+            text += ' ' + message;
+        })
+        $('#logs').append($('<div>').text('[' + level + ']' + text + '\n'));
+    }
+}
+
+function updateFieldsCorrespondingToStreamingProtocol() {
+    var isDASH = $('#protocol').val() === 'DASH';
+    $('#containerFormat').prop('disabled', isDASH);
+    $('#discontinuityMode').prop('disabled', isDASH);
+    $('#displayFragmentNumber').prop('disabled', !isDASH);
+    var DASH_PLAYERS = ['DASH.js', 'Shaka Player'];
+    var HLS_PLAYERS = ['HLS.js', 'Shaka Player', 'VideoJS'];
+
+    var players = isDASH ? DASH_PLAYERS : HLS_PLAYERS;
+    $('#player').empty();
+    players.forEach(function (player) {
+        var option = $('<option>').text(player);
+        $('#player').append(option);
+    });
+    $('#player').trigger('change');
+}
+updateFieldsCorrespondingToStreamingProtocol();
+
+function updateFieldsCorrespondingToPlaybackMode() {
+    var isLive = $('#playbackMode').val() === 'LIVE';
+    $('#startTimestamp').prop('disabled', isLive);
+    $('#endTimestamp').prop('disabled', isLive);
+}
+updateFieldsCorrespondingToPlaybackMode();
+
+$('.loader').hide();
+$('.main').show();
+console.log("Page loaded")
